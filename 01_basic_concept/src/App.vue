ffdc64876d237b24725f28740702389412670b5f
Sourav Samanta <souravthewriter95@gmail.com>
2025-08-05 17:28:06 +0530

    first commit

---
+++ 01_basic_concept/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+	<h1>Today we are discussing how vue works</h1>
+	<h2 class="msg-class">{{ message }}</h2>
+	<input type="text" v-model="name" />
+	<br />
+	<select name="select" id="" v-model="count">
+		<option :value="100">100</option>
+		<option :value="50">50</option>
+		<option :value="10">10</option>
+	</select>
+	<h4>{{ fruitText }}</h4>
+</template>
+
+<script>
+export default {
+	name: "App",
+	data() {
+		return {
+			message: "Hello Vue!",
+			name: "",
+			count: 10,
+		};
+	},
+	computed: {
+		fruitText() {
+			const favrtFrtObj = {
+				gopal: "Mango",
+				anuradha: "Orange",
+				anagha: "Apple",
+			};
+
+			const hasValue = favrtFrtObj[this.name.toLowerCase()];
+
+			if (hasValue) {
+				return `${this.name}'s favorite fruit is ${hasValue}, can eat ${this.count} ${hasValue}`;
+			} else {
+				return "Unknown Name";
+			}
+		},
+
+		// this.[[variables name or computed property or methods name]]  to access or call
+	},
+	mounted() {},
+};
+</script>
+
+<style scoped>
+.msg-class {
+	color: blue;
+}
+h1 {
+	color: green;
+}
+hr {
+	color: yellowgreen;
+}
+</style>
---
+++ 02_basic_concept_2/src/App.vue
@@ -0,0 +1,86 @@
+<template>
+	<h1>You did it!</h1>
+	<!-- <button v-on:click="increaseNum">Click</button> -->
+
+	<!-- <h4>{{ num }}</h4> -->
+	<br />
+	<!-- <div v-on:mouseover="increaseNum"></div> -->
+	<!-- <div @mouseover="increaseNum"></div> -->
+	<br />
+	<!-- <input type="text" name="" id="" @input="increaseNum" /> -->
+
+	<button class="login" @click="handleLogin" v-if="!isLoggedIn">Login</button>
+	<button class="logout" @click="handleLogout" v-else>Logout</button>
+	<br />
+	<!-- <pre>Actual value:{{ isLoggedIn }}</pre> -->
+	<p v-if="authLoading">Processing...</p>
+	<p v-else-if="isLoggedIn">User Logged In</p>
+	<p v-else>User Logged Out</p>
+
+	<br />
+	<div class="condition">
+		<h4 v-if="isLoggedIn">Auth Text(if)</h4>
+	</div>
+	<h4 v-show="isLoggedIn">Auth Text(show)</h4>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			num: 2,
+			isLoggedIn: false,
+			authLoading: false,
+		};
+	},
+	computed: {
+		logText() {
+			if (this.authLoading) {
+				return `User ${this.isLoggedIn ? "Logging Out" : "Logging In"}...`;
+			} else {
+				return `User ${this.isLoggedIn ? "Logged In" : "Logged Out"}!!!`;
+			}
+		},
+	},
+	methods: {
+		increaseNum() {
+			this.num = this.num + 2;
+			this.secondMethod();
+		},
+		secondMethod() {
+			console.log("second Method called");
+		},
+		handleLogin() {
+			this.authLoading = true;
+			setTimeout(() => {
+				this.isLoggedIn = true;
+				this.authLoading = false;
+			}, 2000);
+		},
+		handleLogout() {
+			this.authLoading = true;
+			setTimeout(() => {
+				this.isLoggedIn = false;
+				this.authLoading = false;
+			}, 2000);
+		},
+	},
+};
+</script>
+
+<style scoped>
+/* div {
+	width: 100px;
+	height: 100px;
+	background: brown;
+} */
+.login {
+	background: rgb(0, 128, 179);
+}
+.logout {
+	background: red;
+}
+button {
+	margin: 10px 0px;
+}
+</style>
