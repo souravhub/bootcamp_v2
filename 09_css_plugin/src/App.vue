31d0e2fd0f16234cbe138600bafe8ce6a2cf9e7c
Sourav Samanta <souravthewriter95@gmail.com>
2025-08-27 17:43:05 +0530

    tailwind css

---
+++ 09_css_plugin/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="p-8">
+		<!-- <h1 class="text-[2.5rem] font-semibold text-center text-yellow-700">
+			Hello world!
+		</h1> -->
+		<h1
+			class="text-yellow-700 sm:text-red-700 md:text-blue-700 lg:text-green-700 xl:text-orange-700 text-5xl"
+		>
+			Hello world!
+		</h1>
+		<br />
+		<hr />
+		<h1 class="header">Normal CSS</h1>
+	</div>
+</template>
+
+<script></script>
+<style scoped>
+.header {
+	color: seagreen;
+	font-size: 30px;
+	text-align: center;
+	font-weight: 700;
+}
+</style>
